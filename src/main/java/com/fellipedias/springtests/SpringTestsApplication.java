b72d5382f9907e661b7037433aa861738ec3e1f8
fellipesdias <62446990+fellipesdias@users.noreply.github.com>
2020-07-27 18:40:29 -0300

    Produto e associação muitos para muitos

--- src/main/java/com/fellipedias/springtests/SpringTestsApplication.java
+++ src/main/java/com/fellipedias/springtests/SpringTestsApplication.java
@@ -1,13 +1,49 @@
 package com.fellipedias.springtests;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringTestsApplication {
+import com.fellipedias.springtests.domain.Categoria;
+import com.fellipedias.springtests.domain.Produto;
+import com.fellipedias.springtests.repositories.CategoriaRepository;
+import com.fellipedias.springtests.repositories.ProdutoRepository;
 
+@SpringBootApplication
+public class SpringTestsApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringTestsApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategoria().addAll(Arrays.asList(cat1));
+		p2.getCategoria().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategoria().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 	}
 
 }
